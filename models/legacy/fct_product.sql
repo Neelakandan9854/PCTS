a267d160e19fb8c02984e026725f001378d3a25b
Neelakandan C <neelakandan9854@gmail.com>
2024-01-11 11:29:47 +0000

    partially completed

--- models/legacy/fct_product.sql
+++ models/legacy/fct_product.sql
@@ -1,14 +1,18 @@
 with 
-  product as ( select * from {{ ref('stg_product') }}),
-
-  final as (
-    select 
-    productid,
-    productname,
-    category,
-    price,
-    stockquantity
-    from product 
-  )
-
-  select * from final
+  pro as ( select * from {{ ref('stg_product') }}),
+  sto as ( select * from {{ ref('stg_stock') }}),
+final as(
+  SELECT
+    pro.productid,
+    pro.productname,
+    sto.stockonhand AS stock_quantity,
+    CASE
+        WHEN sto.stockonhand > 0 THEN 'Available'
+        ELSE 'Not Available'
+    END AS availability_status
+FROM
+   pro
+left JOIN
+    sto ON pro.productid = sto.productid
+)
+select * from final
